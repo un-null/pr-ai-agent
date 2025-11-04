a41b60380a70253ace69aeba14442c80e29d1b92
un-null <kawabata.qppq@gmail.com>
2025-11-04 22:29:54 +0700

    💄 build basic chat ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,42 @@
 "use client";
-import { useEffect, useState } from "react";
-import { Button } from "@/components/ui/button";
+
+import { useChat } from "@ai-sdk/react";
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
-  const [message, setMessage] = useState();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("/api/hello");
-      const { message } = await res.json();
-      setMessage(message);
-    };
-    fetchData();
-  }, []);
-
-  if (!message) return <p>Loading...</p>;
+  const [input, setInput] = useState<string>("");
+  const { messages, sendMessage } = useChat();
 
   return (
-    <div>
-      <h1 className="font-bold text-3xl underline">{message}</h1>
-      <Button
-        variant={"outline"}
-        onClick={async () => {
-          const res = await fetch("/api/test", {
-            method: "POST",
-          });
-          const data = await res.json();
-          alert(data.text);
+    <main className="stretch mx-auto flex w-full max-w-md flex-col py-24">
+      {messages.map((message) => (
+        <div key={message.id} className="mb-4 flex whitespace-pre-wrap">
+          {message.role === "user" ? "User: " : "AI: "}
+          {message.parts.map((part, i) => {
+            switch (part.type) {
+              case "text":
+                return <div key={`${message.id}-${i}`}>{part.text}</div>;
+            }
+            return "";
+          })}
+        </div>
+      ))}
+
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          sendMessage({ text: input });
+          setInput("");
         }}
       >
-        Greet
-      </Button>
-    </div>
+        <Input
+          className="fixed bottom-0 mb-8 w-full max-w-md border-neutral-600"
+          value={input}
+          placeholder="Say something..."
+          onChange={(e) => setInput(e.currentTarget.value)}
+        />
+      </form>
+    </main>
   );
 }
